ba7e8085819452bc44eac48ac7bc9fed49893bf2
Richard Lefevre <rlefevre.6505@gmail.com>
2025-12-11 16:07:05 +0000

    chore: basic set up of client and server files

---
+++ client/src/app.js
@@ -0,0 +1,28 @@
+//TODO: collect users' data and send to the server
+
+const reviewForm = document.getElementById("review-form");
+
+function handleSubmit(event) {
+  event.preventDefault; //! This doesnt seem to be working
+  const formDataTemplate = new FormData(reviewForm);
+  const formValues = Object.fromEntries(formDataTemplate);
+  console.log(formValues);
+  fetch("http://localhost:8080/reviews", {
+    //! Once you finish your project, replace your localhost url with the deployed server url from Render
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      body: JSON.stringify({ formValues }),
+    },
+  });
+}
+
+reviewForm.addEventListener("submit", handleSubmit);
+
+//===============================================
+
+//TODO: render users' data on the interface
+
+//fetch the GET route from the server
+
+// render the data using DOM elements (one per piece of data)
---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express, { application } from "express";
+import cors from "cors";
+import { db } from "./dbconnections.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.info(`Server is running in port ${PORT}`);
+});
+
+app.get("/", (req, res) => res.json({ message: "Welcome to the server!" }));
+
+//TODO: a route to CREATE data in the database
+app.post("/reviews", (request, response) => {
+  const reviewForm = request.body.formValues;
+  console.info(reviewForm);
+  // const query = db.query(
+  //   `INSERT INTO hotelreviews (name, location, date, comments) VALUES ($1, $2, $3, $4)`,
+  //   [reviewForm.name, reviewForm.location, reviewForm.date, reviewForm.comments]
+  // );
+  // response.json({ staus: "success", values: reviewForm });
+  // console.log(formValues);
+});
+
+//TODO: a route to READ data from the database
