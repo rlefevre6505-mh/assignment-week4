751fe661ecc0c42c4bf99de02861628d6d7cdbdc
Richard Lefevre <rlefevre.6505@gmail.com>
2025-12-12 15:08:40 +0000

    fix: set up clientside js to render dates from old reviews wihtout additional information for time of day

--- client/src/app.js
+++ client/src/app.js
@@ -33,27 +33,30 @@ reviewForm.addEventListener("submit", handleSubmit);
 
 //TODO: fetch the GET route from the server
 
+const reviewContainer = document.getElementById("previous-reviews");
+
 async function retrieveReviews() {
   const response = await fetch(
     "https://guestbook-server-1j5c.onrender.com/storedreviews"
   );
   const data = await response.json();
-  console.log(data);
   return data;
 }
 
-//TODO: render the data using DOM elements (one per piece of data)
-
-const reviewContainer = document.getElementById("previous-reviews");
-
-function renderReviews(reviewData) {
-  for (let i = 0; i < previousReviews.length; i++) {
+async function renderReviews() {
+  const data = await retrieveReviews();
+  for (let i = 0; i < data.length; i++) {
     const reviewBox = document.createElement("p");
-    reviewBox.textContent = `Review by ${reviewData[i].name} from ${reviewData[i].location}, who stayed wiht us from ${reviewData[i].date_in} tot ${reviewData[i].date_out}: )`;
-
+    const dateInString = data[i].date_in.toString();
+    const dateOutString = data[i].date_out.toString();
+    const dateIn = dateInString.slice(0, 10);
+    const dateOut = dateOutString.slice(0, 10);
+    reviewBox.textContent = `Review by ${data[i].name} from ${data[i].location}, who stayed with us from ${dateIn} to ${dateOut}:`;
     reviewBox.classList.add("old-review");
     reviewBox.id = i;
     reviewContainer.appendChild(reviewBox);
+    //TODO: add function for user to like???
+    console.log(dateIn, dateOut);
   }
 }
 
