c294a8ee3778b1be7e67cfd5a525486a7b370fbb
Richard Lefevre <rlefevre.6505@gmail.com>
2025-12-12 10:09:58 +0000

    chore: updated server url in client side js

--- client/src/app.js
+++ client/src/app.js
@@ -7,7 +7,7 @@ function handleSubmit(event) {
   const formDataTemplate = new FormData(reviewForm);
   const formValues = Object.fromEntries(formDataTemplate);
   console.log(formValues);
-  fetch("http://localhost:8080/reviews", {
+  fetch("https://guestbook-client-2nen.onrender.com/reviews", {
     //! Once you finish your project, replace your localhost url with the deployed server url from Render
     method: "POST",
     headers: {
